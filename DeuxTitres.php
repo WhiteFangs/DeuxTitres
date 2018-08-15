61086c6a0d1aa6d5424833a3dbe7c3f5ddb11827
WhiteFangs <white-fangs@hotmail.fr>
2018-08-15 12:43:14 +0200

    Fix bot after Google News redesign

--- DeuxTitres.php
+++ DeuxTitres.php
@@ -6,11 +6,11 @@ header('Content-Type: text/html; charset=utf-8');
 
 /** Set access tokens here - see: https://apps.twitter.com/ **/
 $APIsettings = array(
-    'oauth_access_token' => $oauthToken,
-    'oauth_access_token_secret' => $oauthTokenSecret,
-    'consumer_key' => $consumerKey,
-    'consumer_secret' => $consumerSecret
-);
+  'oauth_access_token' => $oauthToken,
+  'oauth_access_token_secret' => $oauthTokenSecret,
+  'consumer_key' => $consumerKey,
+  'consumer_secret' => $consumerSecret
+  );
 
 function getCURLOutput($url){
   $ch = curl_init();
@@ -35,15 +35,14 @@ function getDOMXPath($page){
   return $xpath;
 }
 
-function getTopics ($category) {
+function getTopics () {
   $baseUrl = "http://news.google.fr";
   $topics = array();
-  $categoryPage = getCURLOutput($baseUrl . '/news/headlines/section/topic/' . $category . '.fr_fr/?ned=fr&hl=fr');
-  $categoryXpath = getDOMXPath($categoryPage);
-  $topicsName = $categoryXpath->query('//*[@class="iuPoYd"]/c-wiz/a/*[@class="Q3vG6d kzAuJ"]/text()');
-  $topicsNodes = $categoryXpath->query('//*[@class="iuPoYd"]/c-wiz/a[@class="J3nBBd ME7ew"]');
+  $homePage = getCURLOutput('https://news.google.com/?hl=fr&gl=FR&ceid=FR%3Afr'); // Google News FR Home page
+  $homeXpath = getDOMXPath($homePage);
+  $topicsNodes = $homeXpath->query('//a[@class="boy4he"]');
   foreach($topicsNodes as $key => $node) {
-    $topic = (object) array('name' => $topicsName->item($key)->nodeValue, 'url' => $baseUrl . $node->getAttribute("href"));
+    $topic = (object) array('name' => $node->getAttribute("aria-label"), 'url' => $baseUrl . substr($node->getAttribute("href"), 1));
     $topics[] = $topic;
   }
   return $topics;
@@ -52,55 +51,41 @@ function getTopics ($category) {
 function getHeadline($topic){
   $headlines = array();
   $topicPage = getCURLOutput($topic->url);
-  $topicPage = str_replace("<b>", "", $topicPage);
-  $topicPage = str_replace("</b>", "", $topicPage);
+  $topicPage = str_replace("<span>", "", $topicPage);
+  $topicPage = str_replace("</span>", "", $topicPage);
   $topicXpath = getDOMXPath($topicPage);
-  $headlinesNodes = $topicXpath->query('//a[@class="nuEeue hzdq5d ME7ew"]/text()');
+  $headlinesNodes = $topicXpath->query('//a[@class="ipQwMb Q7tWef"]/text()');
   for($i = 0; $i < $headlinesNodes->length; $i++){
     $titletext = $headlinesNodes->item($i)->nodeValue;
     if(strstr($titletext, " ...") === false && strstr($titletext, $topic->name) !== false){
       $headlines[] = $titletext;
     }
   }
-  $index = array_rand($headlines);
-  $headline = $headlines[$index];
+  $headline = $headlines[array_rand($headlines)];
   return $headline;
 }
 
 function tweet(){
   global $APIsettings;
-  $categoryCodes = array('WORLD', 'NATION', 'BUSINESS', 'SCITECH', 'HEALTH', 'ENTERTAINMENT', 'SPORTS');
-  $firstIdx = array_rand($categoryCodes);
-  $firstCat = $categoryCodes[$firstIdx];
-  unset($categoryCodes[$firstIdx]);
-  $categoryCodes = array_values($categoryCodes);
-  $topics = getTopics($firstCat);
-  if(count($topics) > 0){
-    $firstTopic = $topics[array_rand($topics)];
-    $headline = getHeadline($firstTopic);
-    if($headline != null && strstr($headline, $firstTopic->name) !== false){
-      $secondCat = $categoryCodes[array_rand($categoryCodes)];
-      $newTopics = getTopics($secondCat);
-      if(count($newTopics) > 0){
-        $secondTopic = $newTopics[array_rand($newTopics)];
-        $newHeadline = str_replace($firstTopic->name, $secondTopic->name, $headline);
-        if(strlen($newHeadline) < 141){
-
+  $topics = getTopics();
+  $firstIdx = array_rand($topics);
+  $firstTopic = $topics[$firstIdx];
+  unset($topics[$firstIdx]);
+  $topics = array_values($topics);
+  $headline = getHeadline($firstTopic);
+  if($headline != null && strstr($headline, $firstTopic->name) !== false){
+    $secondTopic = $topics[array_rand($topics)];
+    $newHeadline = str_replace($firstTopic->name, $secondTopic->name, $headline);
+    if(strlen($newHeadline) < 281){
           // Post the tweet
-          $postfields = array(
-              'status' =>  $newHeadline);
-          $url = "https://api.twitter.com/1.1/statuses/update.json";
-          $requestMethod = "POST";
-          $twitter = new TwitterAPIExchange($APIsettings);
-          echo $twitter->buildOauth($url, $requestMethod)
-                        ->setPostfields($postfields)
-                        ->performRequest();
-        }else{
-          tweet();
-        }
-      }else{
-        tweet();
-      }
+      $postfields = array(
+        'status' =>  $newHeadline);
+      $url = "https://api.twitter.com/1.1/statuses/update.json";
+      $requestMethod = "POST";
+      $twitter = new TwitterAPIExchange($APIsettings);
+      echo $twitter->buildOauth($url, $requestMethod)
+      ->setPostfields($postfields)
+      ->performRequest();
     }else{
       tweet();
     }
